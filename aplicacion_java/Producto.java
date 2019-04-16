1763f493cc91a18664e43839f6d37e327b1ab0da
guilogar <jdkdejava@gmail.com>
2019-04-16 20:29:35 +0200

    Aplicacion en java parcialmente construida.

---
+++ aplicacion_java/Producto.java
@@ -0,0 +1,92 @@
+import java.util.Map;
+import java.sql.*;
+
+public class Producto
+{
+    private int id;
+    private String code;
+    private ProcessBuilder pb;
+
+    public Producto(int id, String code, ProcessBuilder pb)
+    {
+        this.id = id;
+        this.code = code;
+        this.pb = pb;
+    }
+
+    public void updateAttribute(String[] columnas, String[] valores)
+    {
+        if(columnas.length > 0 && valores.length > 0 && columnas.length == valores.length)
+        {
+            Map<String, String> env = this.pb.environment();
+            String host = env.get('HOST_DB');
+            String dbname = env.get('DB_NAME');
+            String dbuser = env.get('DB_USER');
+            String dbpass = env.get('DB_PASS');
+
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + '/' + dbname, dbuser, dbpass);
+
+            String query = "update products set ";
+            for (int i = 0; i < columnas.length; i++)
+            {
+                query += columnas[i] + " = ? ,"; // + valores[i];
+            }
+            query.substring(0, query.length() - 1);
+            query += " where id = ? and code = ? ";
+
+            PreparedStatement sentencia = conexion.prepareStatement(query);
+            int v = 0;
+            for (v = 0; v < valores.length; v++)
+            {
+                sentencia.setString(v+1, valores[v]);
+            }
+            sentencia.setString(++v, this.id);
+            sentencia.setString(++v, this.code);
+
+            ResultSet rs = sentencia.executeUpdate();
+
+            ResultSet rs = this.getAttributes();
+            this.id = (int) rs.getObject("id");
+            this.code = (String) rs.getObject("code");
+        } else
+        {
+            throw new Exception("Error. Lista de columnas y/o valores incorrecta.");
+        }
+    }
+
+    public ResultSet getAttributes()
+    {
+        Map<String, String> env = this.pb.environment();
+        String host = env.get('HOST_DB');
+        String dbname = env.get('DB_NAME');
+        String dbuser = env.get('DB_USER');
+        String dbpass = env.get('DB_PASS');
+
+        Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + '/' + dbname, dbuser, dbpass);
+
+        String query = "select * from products where id = ? and code = ? ";
+        PreparedStatement sentencia = conexion.prepareStatement(query);
+        sentencia.setString(1, this.id);
+        sentencia.setString(2, this.code);
+        ResultSet rs = sentencia.executeQuery();
+
+        return rs;
+    }
+
+    public void deleteProduct()
+    {
+        Map<String, String> env = this.pb.environment();
+        String host = env.get('HOST_DB');
+        String dbname = env.get('DB_NAME');
+        String dbuser = env.get('DB_USER');
+        String dbpass = env.get('DB_PASS');
+
+        Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + '/' + dbname, dbuser, dbpass);
+
+        String query = "delete from products where id = ? and code = ? ";
+        PreparedStatement sentencia = conexion.prepareStatement(query);
+        sentencia.setString(1, this.id);
+        sentencia.setString(2, this.code);
+        ResultSet rs = sentencia.executeUpdate();
+    }
+}
---
+++ aplicacion_java/VideoClub.java
@@ -0,0 +1,71 @@
+import java.util.Map;
+import java.sql.*;
+
+public class VideoClub
+{
+    private ProcessBuilder pb;
+
+    public Set<Producto> listarProductos()
+    {
+        Set<Producto> listaProductos = new Set<Producto>;
+
+        Map<String, String> env = this.pb.environment();
+        String host = env.get('HOST_DB');
+        String dbname = env.get('DB_NAME');
+        String dbuser = env.get('DB_USER');
+        String dbpass = env.get('DB_PASS');
+
+        Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + '/' + dbname, dbuser, dbpass);
+
+        String query = "select * from products ";
+        PreparedStatement sentencia = conexion.prepareStatement(query);
+        ResultSet rs = sentencia.executeQuery();
+
+        while(rs.next())
+        {
+            int id = (int) rs.getObject("id");
+            String code = (String) rs.getObject("code");
+            Producto p = new Producto(id, code, this.pb);
+            listaProductos.add(p);
+        }
+
+        return listaProductos;
+    }
+
+    public Set<Categoria> listarCategorias()
+    {
+        Set<Producto> listaCategorias = new Set<Categoria>;
+
+        Map<String, String> env = this.pb.environment();
+        String host = env.get('HOST_DB');
+        String dbname = env.get('DB_NAME');
+        String dbuser = env.get('DB_USER');
+        String dbpass = env.get('DB_PASS');
+
+        Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + '/' + dbname, dbuser, dbpass);
+
+        String query = "select * from categories ";
+        PreparedStatement sentencia = conexion.prepareStatement(query);
+        ResultSet rs = sentencia.executeQuery();
+
+        while(rs.next())
+        {
+            int id = (int) rs.getObject("id");
+            Categoria c = new Categoria(id, this.pb);
+            listaCategorias.add(p);
+        }
+
+        return listaProductos;
+    }
+
+    public static void main(String[] args)
+    {
+        this.pb = new ProcessBuilder();
+        Map<String, String> env = this.pb.environment();
+        env.put("HOST_DB", "HOST_DB");
+        env.put("DB_NAME", "DB_NAME");
+        env.put("DB_USER", "DB_USER");
+        env.put("DB_PASS", "DB_PASS");
+
+    }
+}
