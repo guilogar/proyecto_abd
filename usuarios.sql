3aa1e74089dd39cdee4173df24b935993123d306
guilogar <jdkdejava@gmail.com>
2019-05-23 23:02:27 +0200

    Dos usuarios extras añadidos. Que se me habian olvidado hasta ahora.

--- usuarios.sql
+++ usuarios.sql
@@ -5,6 +5,11 @@ grant all privileges on videoclub.* to principal;
 
 --------------------------------------------------
 
+create user principalSecundario identified by 'principalSecundario';
+grant all privileges on videoclub.orders to principalSecundario;
+
+--------------------------------------------------
+
 create user colaborador identified by 'colaborador';
 grant select, update on videoclub.* to colaborador;
 
@@ -17,12 +22,17 @@ grant select on videoclub.* to eventual;
 --------------------------------------------------
 
 create user junior identified by 'junior';
-grant select, insert, update, delete on videoclub.clientes to junior;
+grant select, insert, update, delete on videoclub.clients to junior;
 
 --------------------------------------------------
 
 create user externo identified by 'externo';
-grant select on videoclub.alquileres to externo;
-grant select on videoclub.alquieres_clientes to externo;
+grant select on videoclub.suppliers to externo;
+grant select on videoclub.suppliers_products to externo;
+
+--------------------------------------------------
+
+create user externoSecundario identified by 'externoSecundario';
+grant select on videoclub.statuses to externoSecundario;
 
 --------------------------------------------------
